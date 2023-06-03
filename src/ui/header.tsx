fcd1a4d635313d0cf7d2a410fca374113b8e955e
Alex Bolotskov <mamoruuu@proton.me>
2023-06-03 16:57:51 +0600

    feat: github link

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -1,7 +1,11 @@
 import { FC } from "preact/compat"
+import { Github } from "react-bootstrap-icons"
 
 export const Header: FC = () => {
+  const githubUrl = 'https://github.com/alexathome/viewport'
   return <header class="flex justify-end">
-    
+    <a href={githubUrl} rel="nofollow noreferrer noopener" target="_blank" aria-label="Source code on Github">
+      <Github size={32} title="Source code on Github" />
+    </a>
   </header>
 }
